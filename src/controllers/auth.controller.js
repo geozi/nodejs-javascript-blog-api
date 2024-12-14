c69d265ed9975a2471d612639e1ef98d9bbf471b
geozi <yorgosaz@gmail.com>
2024-12-14 20:03:30 +0200

    Added auth controller and auth messages.

---
+++ src/controllers/auth.controller.js
@@ -0,0 +1,3 @@
+/**
+ * TODO: Develop the auth controller.
+ */
---
+++ src/resources/authMessages.js
@@ -0,0 +1,6 @@
+module.exports = {
+  AUTH_FAILED: "Authentication Failed",
+  AUTH_SUCCESS: "Login Successful",
+  AUTH_HEADER_REQUIRED: "Authorization Header is Required",
+  TOKEN_INVALID: "Invalid Token",
+};
